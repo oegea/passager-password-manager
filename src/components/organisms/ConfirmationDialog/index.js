fcef2340b4c316a7f7d0853dd58275007c036530
Oriol Egea <egea@oriol.im>
2022-01-29 13:30:46 +0100

    Translate confirmation dialog

--- src/components/organisms/ConfirmationDialog/index.js
+++ src/components/organisms/ConfirmationDialog/index.js
@@ -8,18 +8,19 @@ import Dialog from '../../atoms/Dialog/Dialog.js';
 import Title from '../../atoms/Title/index.js';
 // Hooks
 import useDialogConfirmation from '../../../hooks/useDialogConfirmation/index.js';
+import useTranslation from '../../../hooks/useTranslation/index.js';
 
 const ConfirmationDialog = ({onAccept, closeDialog}) => {
-
+    const {t} = useTranslation();
     useDialogConfirmation(closeDialog, onAccept);
 
     return (
         <Dialog onClose={() => closeDialog()}>
-            <Title marginBottom='75px'>Are you sure you want to proceed?</Title>
+            <Title marginBottom='75px'>{t('confirmationDialog.Are you sure you want to proceed?')}</Title>
             
             <ButtonWrapper>
-                <Button label="Cancel" onClick={() => closeDialog()} color="black" backgroundColor="white"/>
-                <Button label="Accept" onClick={() => onAccept()} color="black" backgroundColor="white"/>
+                <Button label={t('common.Cancel')} onClick={() => closeDialog()} color="black" backgroundColor="white"/>
+                <Button label={t('common.Continue')} onClick={() => onAccept()} color="black" backgroundColor="white"/>
             </ButtonWrapper>
         </Dialog>
     );
