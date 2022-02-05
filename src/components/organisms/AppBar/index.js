df4380a325ba298f911aa54f59e31e8fd50eb630
Oriol Egea <egea@oriol.im>
2022-02-05 20:50:10 +0100

    Fix lint

--- src/components/organisms/AppBar/index.js
+++ src/components/organisms/AppBar/index.js
@@ -2,7 +2,7 @@
 import PropTypes from 'prop-types';
 // Atoms
 import Toolbar from '../../atoms/Toolbar/index.js';
-import ToolbarInput from '../../atoms/ToolbarInput/index.js';
+// import ToolbarInput from '../../atoms/ToolbarInput/index.js';
 import Title from '../../atoms/Title/index.js';
 import Button from '../../atoms/Button/index.js';
 // Hooks
