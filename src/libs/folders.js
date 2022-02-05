c81c6137cf1b6f9dac6449c070bcbfadffbf193d
Oriol Egea <oegea@vsn.es>
2022-02-05 12:02:14 +0100

    Feature: Delete passwords before deleting a folder

--- src/libs/folders.js
+++ src/libs/folders.js
@@ -19,23 +19,25 @@ export const deleteFolder = async (folderId) => {
 
 const _deleteRelatedPasswords = async (folderId) => {
     const MAX_ITERATIONS = 1000;
-    const DOCS_PER_ITERATION = 5;
+    const DOCS_PER_ITERATION = 10;
     
     // Iteration
     for (let i = 0; i < MAX_ITERATIONS; i += 1){
         // Check if there are still existing passwords on this folder
         const q = query(collection(db, "folders", folderId, "passwords"), limit(DOCS_PER_ITERATION));
         const querySnapshot = await getDocs(q);
-
-        // If not, exit
-        if (querySnapshot.docs.length < 1)
-            break;
         
         // Delete each of them
         const batch = writeBatch(db);
-        querySnapshot.docs.forEach(doc => batch.delete(`/folders/${folderId}/passwords/${doc.id}`))
+        querySnapshot.docs.forEach(doc => batch.delete(doc.ref))
         await batch.commit();
 
+        // If this was the last iteration
+        if (querySnapshot.docs.length < DOCS_PER_ITERATION)
+            break;
+
+        if ( i === MAX_ITERATIONS - 1)
+            throw new Error('Too many passwords to delete');
     }
 
 }
