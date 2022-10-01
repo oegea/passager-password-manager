aa0e6b0aa514759aca5289123abd29c515b66351
Hamza Rahmani <mrezio450@gmail.com>
2022-10-01 19:41:04 -0400

    Add tests for atoms/Dialog, second is still a WIP

---
+++ src/components/atoms/Dialog/Dialog.test.js
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import AtomDialog from './Dialog.js';
+
+const DIALOG_CHILDREN = 'Hello World';
+
+test('renders a dialog with children', () => {
+  // Arrange
+  render(<AtomDialog>{DIALOG_CHILDREN}</AtomDialog>);
+
+  // Act
+  const childElement = screen.getByText(DIALOG_CHILDREN);
+
+  // Assert
+  expect(childElement).toBeInTheDocument();
+});
+
+test('dialog should close when the background is clicked', () => {
+  const handleClose = jest.fn();
+
+  // Arrange
+  render(
+    <div data-isdialogbackground="true">
+      background
+      <AtomDialog onClose={handleClose}>{DIALOG_CHILDREN}</AtomDialog>
+    </div>
+  );
+
+  const dialogBackground = screen.getByText('background');
+  expect(dialogBackground).toBeInTheDocument();
+
+  // Act
+  fireEvent.click(dialogBackground);
+
+  // Assert
+  expect(handleClose).toHaveBeenCalledTimes(1);
+});
