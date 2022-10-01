0dc3b09c92686e35859c9899eaa3eba61a3d08e8
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-10-01 18:19:32 -0300

    test: add InputLabel atom component test

---
+++ src/components/atoms/InputLabel/InputLabel.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import InputLabel from '.';
+
+const children = 'Hello!';
+
+const setupTest = () => {
+  return render(
+    <>
+      <InputLabel htmlFor='username'>
+        <span>
+          {children}
+        </span>
+      </InputLabel>
+      <input id="username" type='text' />
+    </>
+  );
+}
+
+ 
+describe('<InputLabel />', () => {
+  it('should be able to render the label children', () => {
+    setupTest()
+
+    const childrenText = screen.getByText(children);
+
+    expect(childrenText).toBeInTheDocument();
+  });
+
+  it('should be able to connect the label with the input by htmlFor', () => {
+    setupTest()
+
+    const input = screen.getByLabelText(children)
+    expect(input.type).toBe('text')
+  });
+})
+ 
+ 
