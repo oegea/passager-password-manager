c38d589bed11803e3feae9c0ae84c1a8e0f9585f
Oriol Egea <egea@oriol.im>
2023-07-06 21:32:47 +0200

    Minor changes to publish apps to the store

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -58,9 +58,11 @@ const PageLogin = () => {
                         ]
                     }
                 </p>
-                <p style={{color: 'red'}}>
-                    {t('login.Login with Google will be removed soon! Please start using Passager locally or')} <a href="https://github.com/oegea/passager-password-manager/blob/main/backend/readme.md" rel="noreferrer" target="_blank">{t('login.in your own server')}.</a>
-                </p>
+                {!isMobileDevice() ? (
+                    <p style={{color: 'red'}}>
+                        {t('login.Login with Google will be removed soon! Please start using Passager locally or')} <a href="https://github.com/oegea/passager-password-manager/blob/main/backend/readme.md" rel="noreferrer" target="_blank">{t('login.in your own server')}.</a>
+                    </p>
+                ) : null}
                 {!isMobileDevice() ? (
                     <ButtonWrapper justifyContent="center">
                         <Button
